76bfd4a051bf9dbc16ee0d8702523a5a3a70eb6f
David Hong <hongymagic@users.noreply.github.com>
2025-02-18 12:34:33 +1100

    build: deploy previews (#1)
    * build: deploy previews
    
    * build: deploy previews
    
    ---------
    
    Co-authored-by: hongymagic <david.hong4@cba.com.au>

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -13,7 +13,7 @@ const config: Config = {
   url: "https://docusaurus-preview-test.hon.gy",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: process.env.PR_NUMBER ? `/pr${process.env.PR_NUMBER}/` : "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
